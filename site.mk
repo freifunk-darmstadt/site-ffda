6e17403c3131d12d18553b5a907bd8c995d92f7f
martin@seraph <mweinelt@gmail.com>
2014-04-03 01:38:19 +0200

    Initial Commit Erste Version einer Site-Configuration, mit Dank an neoraider und tcatm für ihre Arbeit an gluon und die Erklärung der einzigen Konfigurationsoptionen.

---
+++ site.mk
@@ -0,0 +1,16 @@
+GLUON_SITE_PACKAGES := \
+	gluon-alfred \
+	gluon-autoupdater \
+	gluon-config-mode \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-next-node \
+	gluon-mesh-batman-adv \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-status-page \
+	iwinfo \
+	iptables \
+	haveged
