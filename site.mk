c9327728b05cec453db59ba355da10546229c5ca
Martin Weinelt <mweinelt@gmail.com>
2014-06-30 21:29:10 +0200

    version 0.4.1

--- site.mk
+++ site.mk
@@ -15,7 +15,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.4+0-exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.4+1-exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
