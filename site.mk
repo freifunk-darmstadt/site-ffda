6c157302b8f4a542f12231427693908d4f21138d
Martin Weinelt <mweinelt@gmail.com>
2015-10-15 15:25:13 +0200

    site.mk/x86_generic: add usb support, enables USB NIC usage

--- site.mk
+++ site.mk
@@ -26,6 +26,18 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
+ifeq ($(GLUON_TARGET,x86-generic))
+# support the usb stack on x86 devices
+# and add a few common USB NICs
+GLUON_SITE_PACKAGES += \
+	kmod-usb-core \
+	kmod-usb2 \
+	kmod-usb-hid \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-dm9601-ether
+endif
+
 DEFAULT_GLUON_RELEASE := 0.7.2-$(shell date '+%Y%m%d')
 DEFAULT_GLUON_PRIORITY := 0
 
