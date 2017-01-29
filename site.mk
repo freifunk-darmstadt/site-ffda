866dcee09e9d467d3b9e7b637a640e6e0317e7fb
Martin Weinelt <martin@darmstadt.freifunk.net>
2017-01-29 01:37:37 +0100

    site.mk: add usb/pci packages to x86/x86_64/brcm/ar71xx/mpc85xx targets and devices

--- site.mk
+++ site.mk
@@ -1,3 +1,19 @@
+DEFAULT_GLUON_RELEASE := 0.10.0~$(shell date '+%Y%m%d')
+DEFAULT_GLUON_PRIORITY := 0
+
+# languages to include in images
+GLUON_LANGS ?= en de
+
+# region information for regulatory compliance
+GLUON_REGION ?= eu
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+GLUON_PRIORITY ?= ${DEFAULT_GLUON_PRIORITY}
+
+# Prefer ath10k firmware with 802.11s support
+GLUON_ATH10K_MESH ?= 11s
+
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
 	gluon-respondd \
@@ -28,30 +44,142 @@ GLUON_SITE_PACKAGES := \
 	haveged \
 	ffho-ebtables-net-rules
 
-ifeq ($(GLUON_TARGET),x86-generic)
-# support the usb stack on x86 devices
-# and add a few common USB NICs
-GLUON_SITE_PACKAGES += \
+# basic support for USB stack
+USB_PACKAGES_BASIC := \
 	kmod-usb-core \
 	kmod-usb2 \
+	kmod-usb3 \
+	usbutils
+
+# support for USB input devices
+USB_PACKAGES_HID := \
 	kmod-usb-hid \
+	kmod-hid-generic
+
+# storage support for USB devices
+USB_PACKAGES_STORAGE := \
+	block-mount \
+	blkid \
+	kmod-fs-ext4 \
+	kmod-fs-vfat \
+	kmod-usb-storage \
+	kmod-usb-storage-extras \
+	kmod-nls-cp1250 \
+	kmod-nls-cp1251 \
+	kmod-nls-cp437 \
+	kmod-nls-cp775 \
+	kmod-nls-cp850 \
+	kmod-nls-cp852 \
+	kmod-nls-cp866 \
+	kmod-nls-iso8859-1 \
+	kmod-nls-iso8859-13 \
+	kmod-nls-iso8859-15 \
+	kmod-nls-iso8859-2 \
+	kmod-nls-koi8r \
+	kmod-nls-utf8 \
+	swap-utils
+
+# network support for USB devices
+USB_PACKAGES_NET := \
+	kmod-ath9k-htc  \
+	kmod-ath9k-common \
+	kmod-ath \
+	kmod-brcmfmac \
+	kmod-carl9170 \
+	kmod-mii \
+	kmod-nls-base \
+	kmod-rt73-usb \
+	kmod-rtl8192cu \
+	kmod-rtl8187 \
 	kmod-usb-net \
 	kmod-usb-net-asix \
-	kmod-usb-net-dm9601-ether
+	kmod-usb-net-asix-ax88179 \
+	kmod-usb-net-cdc-eem \
+	kmod-usb-net-cdc-ether \
+	kmod-usb-net-cdc-mbim \
+	kmod-usb-net-cdc-ncm \
+	kmod-usb-net-cdc-subset \
+	kmod-usb-net-dm9601-ether \
+	kmod-usb-net-hso \
+	kmod-usb-net-huawei-cdc-ncm \
+	kmod-usb-net-ipheth \
+	kmod-usb-net-kalmia \
+	kmod-usb-net-kaweth \
+	kmod-usb-net-mcs7830 \
+	kmod-usb-net-pegasus \
+	kmod-usb-net-qmi-wwan \
+	kmod-usb-net-rndis \
+	kmod-usb-net-rtl8152 \
+	kmod-usb-net-sierrawireless \
+	kmod-usb-net-smsc95xx
+
+# bnx2: BCM5706/5708/5709/5716 (Broadcom NetExtreme)
+# e1000: Intel(R) PRO/1000
+# forceeth: NVIDIA nForce
+# igb: Intel(R) 82575/82576 PCI-Express
+# natsemi: National Semiconductor DP8381x series
+# r8169: RTL-8169 PCI
+# via-rhine: Via Rhine
+PCI_PACKAGES_NET := \
+	kmod-bnx2 \
+	kmod-e1000 \
+	kmod-forcedeth \
+	kmod-igb \
+	kmod-natsemi \
+	kmod-r8169 \
+	kmod-via-rhine \
+
+ifeq ($(GLUON_TARGET),x86-generic)
+GLUON_SITE_PACKAGES += \
+	$(USB_PACKAGES_BASIC) \
+	$(USB_PACKAGES_HID) \
+	$(USB_PACKAGES_STORAGE) \
+	$(USB_PACKAGES_NET) \
+	$(PCI_PACKAGES_NET)
 endif
 
-DEFAULT_GLUON_RELEASE := 0.10.0~$(shell date '+%Y%m%d')
-DEFAULT_GLUON_PRIORITY := 0
+ifeq ($(GLUON_TARGET),x86-64)
+GLUON_SITE_PACKAGES += \
+	$(USB_PACKAGES_BASIC) \
+	$(USB_PACKAGES_HID) \
+	$(USB_PACKAGES_STORAGE) \
+	$(USB_PACKAGES_NET) \
+	$(PCI_PACKAGES_NET)
+endif
 
-# languages to include in images
-GLUON_LANGS ?= en de
+#  Raspberry Pi A/B/B+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
+GLUON_SITE_PACKAGES += \
+	$(USB_PACKAGES_BASIC) \
+	$(USB_PACKAGES_HID) \
+	$(USB_PACKAGES_STORAGE) \
+	$(USB_PACKAGES_NET)
+endif
 
-# region information for regulatory compliance
-GLUON_REGION ?= eu
+# Raspberry Pi 2
+ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
+GLUON_SITE_PACKAGES += \
+	$(USB_PACKAGES_BASIC) \
+	$(USB_PACKAGES_HID) \
+	$(USB_PACKAGES_STORAGE) \
+	$(USB_PACKAGES_NET) \
+endif
 
-# Allow overriding the release number from the command line
-GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
-GLUON_PRIORITY ?= ${DEFAULT_GLUON_PRIORITY}
 
-# Prefer ath10k firmware with 802.11s support
-GLUON_ATH10K_MESH ?= 11s
+# ar71xx-generic
+GLUON_ARCHERC5_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(USB_PACKAGES_STORAGE) $(USB_PACKAGES_NET)
+GLUON_ARCHERC7_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(USB_PACKAGES_STORAGE) $(USB_PACKAGES_NET)
+GLUON_DIR505A1_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(USB_PACKAGES_STORAGE) $(USB_PACKAGES_NET)
+GLUON_TLWDR3600_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(USB_PACKAGES_STORAGE) $(USB_PACKAGES_NET)
+GLUON_TLWDR4300_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(USB_PACKAGES_STORAGE) $(USB_PACKAGES_NET)
+GLUON_TLWR1043_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(USB_PACKAGES_STORAGE) $(USB_PACKAGES_NET)
+GLUON_TLWR2543_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(USB_PACKAGES_STORAGE) $(USB_PACKAGES_NET)
+GLUON_TLWR842_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(USB_PACKAGES_STORAGE) $(USB_PACKAGES_NET)
+GLUON_WNDR3700_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(USB_PACKAGES_STORAGE) $(USB_PACKAGES_NET)
+GLUON_WRT160NL_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(USB_PACKAGES_STORAGE) $(USB_PACKAGES_NET)
+GLUON_WZRHPAG300H_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(USB_PACKAGES_STORAGE) $(USB_PACKAGES_NET)
+GLUON_WZRHPG450H_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(USB_PACKAGES_STORAGE) $(USB_PACKAGES_NET)
+
+# mpc85xx.generic
+GLUON_TLWDR4900_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(USB_PACKAGES_STORAGE) $(USB_PACKAGES_NET)
+
