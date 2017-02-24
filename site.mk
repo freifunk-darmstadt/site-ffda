2fcff92873f59a0464382cb0a84434f1156c7e54
Martin Weinelt <mweinelt@gmail.com>
2017-02-24 02:14:07 +0100

    bump to 0.9.4

--- site.mk
+++ site.mk
@@ -39,7 +39,7 @@ GLUON_SITE_PACKAGES += \
 	kmod-usb-net-dm9601-ether
 endif
 
-DEFAULT_GLUON_RELEASE := 0.9.3-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.9.4-$(shell date '+%Y%m%d')
 DEFAULT_GLUON_PRIORITY := 0
 
 # languages to include in images
