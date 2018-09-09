7090f235d624e515c4edbf6f5652df59b422d86b
David Bauer <mail@david-bauer.net>
2018-09-09 13:16:37 +0200

    add ffda-domain-director package

--- site.mk
+++ site.mk
@@ -39,6 +39,7 @@ GLUON_FEATURES := \
 
 # Additional packages to install on every image
 GLUON_SITE_PACKAGES := \
+	ffda-domain-director \
 	ffda-migrate-update-branch \
 	ffda-update-stabilizer \
 	iwinfo \
