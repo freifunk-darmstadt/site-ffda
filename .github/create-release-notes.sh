5ab27e286cf615317af89688022d37b0dd4f8411
David Bauer <mail@david-bauer.net>
2023-11-21 00:26:50 +0100

    ci: ignore shellcheck style recommendation
    Signed-off-by: David Bauer <mail@david-bauer.net>
    (cherry picked from commit 3861bb239ac0eea9a84ccbe3078a6e59c0efc21a)

--- .github/create-release-notes.sh
+++ .github/create-release-notes.sh
@@ -6,6 +6,8 @@ MANIFEST_PATH="$1"
 OUTPUT_PATH="$2"
 
 echo "## build-meta" > "$OUTPUT_PATH"
+
+# shellcheck disable=SC2129
 echo "\`\`\`" >> "$OUTPUT_PATH"
 cat "$MANIFEST_PATH" >> "$OUTPUT_PATH"
 echo "\`\`\`" >> "$OUTPUT_PATH"
